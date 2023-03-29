39cf1ca4882a486e74e7af9a7260f6dbd443c41a
ARCHIT VERMA <125059938+Archit-09AV@users.noreply.github.com>
2023-03-29 10:16:51 -0400

    Add files via upload

---
+++ testingstreamlit.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+from transformers import pipeline
+
+st.title('Sentiment Analyser App')
+st.write('This app uses the Hugging Face Transformers [sentiment analyser](https://huggingface.co/course/chapter1/3?fw=tf) library to clasify the sentiment of your input as postive or negative. The web app is built using [Streamlit](https://docs.streamlit.io/en/stable/getting_started.html).')
+st.write(
+    'To find out how this app was developed, please check out my [Medium post](https://medium.com/@rtkilian/deploy-and-share-your-sentiment-analysis-app-using-streamlit-sharing-2ba3ca6a3ead). To see my source code, have a look at my [GitHub repo](https://github.com/rtkilian/streamlit-huggingface).')
+
+st.write('*Note: it will take up to 30 seconds to run the app.*')
+
+form = st.form(key='sentiment-form')
+user_input = form.text_area('Enter your text')
+submit = form.form_submit_button('Submit')
+
+if submit:
+    classifier = pipeline("sentiment-analysis")
+    result = classifier(user_input)[0]
+    label = result['label']
+    score = result['score']
+
+    if label == 'POSITIVE':
+        st.success(f'{label} sentiment (score: {score})')
+    else:
+        st.error(f'{label} sentiment (score: {score})')
+
+
+# In[ ]:
+
+
+
+
